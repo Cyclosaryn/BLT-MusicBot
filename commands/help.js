8525b3b6d8a214d16bb3674d27ac98ef6b3eb774
Cyclosaryn <mcj.verbroekken@gmail.com>
2024-10-27 18:16:44 +0100

    Refactor help command to update command list and author information

--- commands/help.js
+++ commands/help.js
@@ -8,15 +8,15 @@ module.exports = {
 	async execute(interaction) {
 		const embed = new EmbedBuilder()
             .setColor(0x0099FF)
-			.setTitle('Lijst van commando\'s')
-            .setAuthor({ name: 'Cyclosarin', iconURL: 'https://i.ibb.co/mCJFKrH/Discord-Picture.jpg', url: 'https://discord.com/users/269195416963121152' })
+			.setTitle('List of commands')
+            .setAuthor({ name: 'Developer: Cyclosaryn', iconURL: 'https://i.ibb.co/mCJFKrH/Discord-Picture.jpg', url: 'https://discord.com/users/269195416963121152' })
 			.setDescription('This is a list with all currently available commands.')
 			.addFields(
                 { name: '/playstream', value: 'Allows to set a voice channel and a stream to play that stream in, will default to a stream if no stream is supplied.' },
 				{ name: '/help', value: 'Shows this list' },
             )
             .setTimestamp()
-			.setFooter({ text: '© 🎵BLT - MusicBot🎵 by Cyclosarin', iconURL: 'https://cdn.discordapp.com/app-icons/1195694045519945748/29e47d0d61763734c3118312cf44f516.webp?size=96' });
+			.setFooter({ text: '© 🎵BLT - MusicBot🎵 by Cyclosaryn', iconURL: 'https://cdn.discordapp.com/app-icons/1195694045519945748/29e47d0d61763734c3118312cf44f516.webp?size=96' });
 
 		await interaction.reply({ embeds: [embed] });
 	},
